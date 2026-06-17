f03a1b8398cf2dfb294317575093aeee2cc15a2a
Aditya Kumar <adityakum2004@gmail.com>
2026-06-17 13:41:45 +0530

    added for it

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void testLogin() {
+
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://the-internet.herokuapp.com/login");
+
+        System.out.println("Page Opened");
+
+        driver.quit();
+    }
+}
